bd4f368420b270b254b786d75328972e6e72119e
Brittan McGinnis <konamax123@gmail.com>
2014-04-18 15:31:44 -0700

    some controller testing all tests pass

--- spec/index_controller_spec.rb
+++ spec/index_controller_spec.rb
@@ -1,36 +1,51 @@
 require 'spec_helper'
 
 # describe "IndexController (this is a skeleton controller test!)" do
-  describe 'get new moment' do
-    it 'should go to gallery page' do
-      get "/gallery"
-      expect(last_response).to be_ok
-    end
+describe 'galleries' do
+  # before(:all) do 
+  #   Scraper::Client.any_instance.stub(:data) {
+  #     [{
+  #         photographer: "Brittan McGinnis",
+  #         tags: "Dog",
+  #         title: "Great dog photo",
+  #         url: "http://ppcdn.500px.org/67368609/4145fa1c574512093798dda08573ee3f1a7b531b/4.jpg" #.gsub("/2.jpg", "/5.jpg")
+  #       }]
+  #   }
+  # end
+  before(:each) do 
+    Photo.destroy_all
+    Primary.destroy_all
   end
 
-#   describe 'get all bands' do
-#     it 'should see all bands' do
-#       get "/bands"
-#       expect(last_response).to be_ok
-#     end
-#   end
+  it 'should go to index page' do
+    get "/"
+    expect(last_response).to be_ok
+  end
 
-#   describe 'create a new band' do
-#     band_name = 'chromatics'
-#     new_params = {
-#         name: band_name
-#       }
-#     new_session = {
-#       'rack.session' => {
-#         # Could preload stuff into the session here...
-#       }
-#     }
-#     it 'should add a new band' do
-#       expect{
-#         post('/bands', new_params, new_session)
-#       }.to change(Band, :count).by(1)
-#       last_response.should be_ok
-#       last_response.body.should == band_name
-#     end
-#   end
-# end
+
+  describe 'the gallery page' do 
+    context "with invalid params" do 
+      it "should throw an error" do 
+        expect{get "/gallery"}.to raise_error
+      end
+    end
+    context "with valid params" do 
+      it "should give a page" do 
+        expect{
+          get "/gallery", {tag_search: "happy"}
+          expect(last_response).to be_ok
+        }.not_to raise_error
+      end
+      it "should update the photo count" do 
+        expect{
+          get "/gallery", {tag_search: "dog"}
+          expect(last_response).to be_ok
+        }.to change(Photo ,:count)
+      end
+    end
+  end
+end
+
+
+
+
