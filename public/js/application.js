928a1661a80a7d98e0252ddc2058eef2540a80ca
Ravi S. Rāmphal <rramphal@gmail.com>
2014-04-17 22:30:54 -0700

    base styling

--- public/js/application.js
+++ public/js/application.js
@@ -1,8 +1,22 @@
-$(document).ready(function() {
-  var thing = moments.pop()
-  $('img').attr('src', thing)
-  $('body').on('click', ".nextPhoto", function(){
-    var moment = moments.pop()
-    $('img').attr('src', moment)
-  })
-});
+$(window).resize(center_elements);
+
+function center(element, rel_elt){
+  element = element || ("#container")
+  rel_elt = rel_elt || $(element).parent()
+
+  $(element).css({
+      position:'absolute',
+      left: ($(rel_elt).width()  - $(element).outerWidth())/2,
+      top:  ($(rel_elt).height() - $(element).outerHeight())/2
+  });
+}
+
+function center_elements() { 
+    center("#container", window);
+    center("#seed_form");
+  }
+
+$(document).ready(function(){
+  center_elements();
+  $("#seed_input").focus().val("")
+})
